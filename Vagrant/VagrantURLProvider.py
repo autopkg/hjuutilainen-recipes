5dfa299abb84feeb15761ac0b4561be81b57da72
Hannes Juutilainen <hjuutilainen@mac.com>
2013-12-10 11:12:11 +0200

    Updated Vagrant URL provider to support the new download page

--- Vagrant/VagrantURLProvider.py
+++ Vagrant/VagrantURLProvider.py
@@ -23,10 +23,9 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["VagrantURLProvider"]
 
 
-MAIN_DOWNLOAD_URL = "http://downloads.vagrantup.com"
+MAIN_DOWNLOAD_URL = "http://www.vagrantup.com/downloads.html"
 
-re_tag_url = re.compile(r'<a class="tag" href=[\'\"](?P<tag_url>http://downloads.vagrantup.com/tags/v[0-9\.]+)[\'\"]>', re.IGNORECASE)
-re_dmg_url = re.compile(r'<a class="file type-dmg" href=[\'\"](?P<dmg_url>.*Vagrant-[0-9\.]+\.dmg)[\'\"]>', re.IGNORECASE)
+re_dmg_url = re.compile(r'<a href=[\'\"](?P<dmg_url>https://dl.bintray.com/mitchellh/vagrant/Vagrant-[0-9\.]+\.dmg)[\'\"]>', re.IGNORECASE)
 
 class VagrantURLProvider(Processor):
     """Provides a download URL for the latest Vagrant"""
@@ -43,7 +42,7 @@ class VagrantURLProvider(Processor):
     }
     description = __doc__
     
-    def parse_tag_url(self, base_url):
+    def parse_download_url(self, base_url):
         """Returns the URL"""
         try:
             f = urllib2.urlopen(base_url)
@@ -63,35 +62,11 @@ class VagrantURLProvider(Processor):
         return dmg_url
     
     
-    def parse_download_url(self, base_url):
-        """Returns the URL"""
-        try:
-            f = urllib2.urlopen(base_url)
-            html = f.read()
-            f.close()
-        except BaseException as e:
-            raise ProcessorError("Can't download %s: %s" % (base_url, e))
-
-        m = re_tag_url.search(html)
-
-        if not m:
-            raise ProcessorError(
-                "Couldn't find tag link in %s" % base_url)
-        
-        tag_url = m.group("tag_url")
-        self.output("Tag link is %s" % tag_url)
-        return tag_url
-    
-    
     def get_vagrant_dmg_url(self, base_url):
         """Find and return a download URL"""
         
-        # Parse the main download page to get
-        # a link to the latest version
-        tag_url = self.parse_download_url(base_url)
-        
-        # Parse the "tag" page to get a dmg link
-        dmg_url = self.parse_tag_url(tag_url)
+        # Parse the download page to get a dmg link
+        dmg_url = self.parse_download_url(base_url)
         
         return dmg_url
     
