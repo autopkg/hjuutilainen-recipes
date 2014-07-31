0a7c0996a22de0a0db5e44f9551fbfe1df23ba59
cgerke <chris.gerke@gmail.com>
2014-07-31 14:56:18 +1000

    Processor change
    Updated the processor with better error checking, slightly changed the
    input variable names to make them more logical as this process (and
    CMApUtil) can accept app and dmg files along with PKG files for source.

--- OpenOffice/cmmacCreator.py
+++ OpenOffice/cmmacCreator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2014 Chris Gerke
+# Copyright 2013 Timothy Sutton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,46 +14,96 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import shutil
-import subprocess
+# Borrowed code and concepts from Unzipper and Copier processors.
 
+# chris.gerke@gmail.com
+# Borrowed code and concepts from FlatPkgUnpacker.py in the AutoPKG core. Credits above ^
+# see comments throughout, there is a some todo items
+# figure out how to make this a shared processor
+
+import os.path
+import subprocess
+import shutil
+
+from glob import glob
 from autopkglib import Processor, ProcessorError
 
 __all__ = ["cmmacCreator"]
 
 class cmmacCreator(Processor):
-    '''Create an SCCM cmmac file using a pkg.'''
-
-    description = __doc__
-
+    description = ("Compresses an app, dmg or pkg using CMAppUtil. ")
     input_variables = {
-        'source_pkg': {
-            'description': 'Path to a package',
-            'required': True,
+        "source_file": {
+            "required": True,
+            "description": ("Path to an app, dmg or pkg. "),
         },
-        'destination_folder': {
-            'description': 'Path to the cmmac file to be created',
-            'required': True,
+        "destination_directory": {
+            "required": True,
+            "description": ("Directory where the cmmac will be created "),
         },
     }
+    output_variables = {
+    }
 
-    output_variables = {}
+    __doc__ = description
+    source_path = None
 
-    def compress(self, source_file, dest_path):
+    def cmmacConvert(self):
+        # CMAppUtil is required http://www.microsoft.com/en-us/download/details.aspx?id=36212
+        if os.path.exists('/usr/local/bin/CMAppUtil'):
+            try:
+                self.output("Found binary %s" % '/usr/local/bin/CMAppUtil')
+            except OSError as e:
+                raise ProcessorError(
+                    "Can't find binary %s: %s" % ('/usr/local/bin/CMAppUtil', e.strerror))
         try:
-            subprocess.check_call(['CMAppUtil','-s','-v','-c', source_file, '-o', dest_path])
-        except subprocess.CalledProcessError, err:
-            raise ProcessorError("%s compressing %s" % (err, source_file))
+            cmmaccmd = ["/usr/local/bin/CMAppUtil",
+                      "-s",
+                      "-v",
+                      "-c", self.env['source_file'],
+                      "-o", self.env['destination_directory']]
+            p = subprocess.Popen(cmmaccmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            (out, err) = p.communicate()
+        except OSError as e:
+            raise ProcessorError("cmmac execution failed with error code %d: %s"
+                % (e.errno, e.strerror))
+        if p.returncode != 0:
+            raise ProcessorError("cmmac conversion of %s failed: %s"
+                % (self.env['source_file'], err))
 
     def main(self):
-        source_file = self.env.get('source_pkg')
-        dest_path = self.env.get('destination_folder')
+        if os.path.exists(self.env['source_file']):
+            try:
+                self.output("Found %s" % self.env['source_file'])
+            except OSError as e:
+                raise ProcessorError(
+                    "Can't find %s" % (self.env['source_file'], e.strerror))
 
-        self.compress(source_file, dest_path)
+        # probably don't need this because most people will use the RECIPE_CACHE_DIR
+        # which should be the parent directory for the source_file, check above should catch this
+        if os.path.exists(self.env['destination_directory']):
+            try:
+                self.output("Found %s" % self.env['destination_directory'])
+            except OSError as e:
+                raise ProcessorError(
+                    "Can't find %s" % (self.env['destination_directory'], e.strerror))
 
-        self.output("Compressed for SCCM %s to %s"
-            % (source_file, dest_path))
+        # overwrite cmmac? or simply raise exception?
+        # cmmac files can be extracted using tar -xvf, the resources contain a metadata file and the PKG/APP
+        # its possible with some more code to check and compare the versions of those items and make a decision
+        # thinking about this after I get a little better with python
+        # figure out how to append to "Nothing downloaded, packaged or imported." so it reads...
+        # Nothing downloaded, packaged, converted or imported.
+        sourcefilebase = os.path.basename(self.env['source_file'])
+        cmmac = (self.env['destination_directory'] + "/" + sourcefilebase + ".cmmac")
+
+        if os.path.exists(cmmac):
+              self.output("Found an existing cmmac %s exiting..." % cmmac)
+        else:
+              self.output("Didn't find an existing cmmac %s" % cmmac)
+              self.cmmacConvert()
 
 if __name__ == '__main__':
     processor = cmmacCreator()
